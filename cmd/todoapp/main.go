d3fa864bd7d89aeb2471881edcd99c2f6617ea51
paind <paindex8@gmail.com>
2026-07-17 18:43:11 +0500

    Add feature tasks
    - create task
    - read task
    - change task
    - delete task

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -6,18 +6,28 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	core_logger "github.com/nurassul/todoapp-golang/internal/core/logger"
 	"github.com/nurassul/todoapp-golang/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/nurassul/todoapp-golang/internal/core/transport/http/middleware"
 	core_http_server "github.com/nurassul/todoapp-golang/internal/core/transport/http/server"
+	tasks_postgres_repository "github.com/nurassul/todoapp-golang/internal/features/tasks/repository/postgres"
+	tasks_service "github.com/nurassul/todoapp-golang/internal/features/tasks/service"
+	tasks_transport "github.com/nurassul/todoapp-golang/internal/features/tasks/transport/http"
 	users_postgres_repository "github.com/nurassul/todoapp-golang/internal/features/users/repository/postgres"
 	users_service "github.com/nurassul/todoapp-golang/internal/features/users/service"
 	users_transport_http "github.com/nurassul/todoapp-golang/internal/features/users/transport/http"
 	"go.uber.org/zap"
 )
 
+var (
+	timeZone = time.UTC
+)
+
 func main() {
+	time.Local = timeZone
+
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
 		syscall.SIGINT, syscall.SIGTERM,
@@ -30,6 +40,8 @@ func main() {
 		os.Exit(1)
 	}
 	defer logger.Close()
+
+	logger.Debug("application time zone", zap.Any("zone", timeZone))
 
 	logger.Debug("initializing postgres connection pool")
 	pool, err := core_pgx_pool.NewPool(
@@ -46,6 +58,11 @@ func main() {
 	usersService := users_service.NewUsersService(usersRepository)
 	usersTransportHTTP := users_transport_http.NewUsersHTTPHandler(usersService)
 
+	logger.Debug("initializing feature", zap.String("feature", "tasks"))
+	tasksRepository := tasks_postgres_repository.NewTasksRepository(pool)
+	tasksService := tasks_service.NewTasksService(tasksRepository)
+	tasksTransportHTTP := tasks_transport.NewTasksHTTPHandler(tasksService)
+
 	logger.Debug("initializing HTTP server")
 	httpServer := core_http_server.NewHTTPServer(
 		core_http_server.NewConfigMust(),
@@ -57,6 +74,7 @@ func main() {
 	)
 	apiVersionRouter := core_http_server.NewAPIVersionRouter(core_http_server.ApiVersion1)
 	apiVersionRouter.RegisterRoutes(usersTransportHTTP.Routes()...)
+	apiVersionRouter.RegisterRoutes(tasksTransportHTTP.Routes()...)
 	httpServer.RegisterAPIRouters(apiVersionRouter)
 
 	if err := httpServer.Run(ctx); err != nil {
---
+++ internal/features/tasks/repository/postgres/models.go
@@ -0,0 +1,37 @@
+package tasks_postgres_repository
+
+import (
+	"time"
+
+	"github.com/nurassul/todoapp-golang/internal/core/domain"
+)
+
+type TaskModel struct {
+	ID           int
+	Version      int
+	Title        string
+	Description  *string
+	Completed    bool
+	CreatedAt    time.Time
+	CompletedAt  *time.Time
+	AuthorUserID int
+}
+
+func taskDomainsFromModels(taskModels []TaskModel) []domain.Task {
+	domains := make([]domain.Task, len(taskModels))
+
+	for i, model := range taskModels {
+		domains[i] = domain.NewTask(
+			model.ID,
+			model.Version,
+			model.Title,
+			model.Description,
+			model.Completed,
+			model.CreatedAt,
+			model.CompletedAt,
+			model.AuthorUserID,
+		)
+	}
+
+	return domains
+}
